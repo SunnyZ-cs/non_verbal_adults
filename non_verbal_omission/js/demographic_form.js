188070ad16d3bfafb77ff1ea0da38f60457dfc92
Sunny <sunny@Sunnys-MacBook-Pro.local>
2026-06-04 22:53:58 -0700

    Set up non_verbal_omission study matching pilot1 reference

---
+++ non_verbal_omission/js/demographic_form.js
@@ -0,0 +1,51 @@
+const demographic_form = {
+    type: jsPsychSurveyHtmlForm,
+    data: {
+        "page_type": "participant_survey",
+    },
+    html: '<div style="max-width:700px; text-align:center; margin: auto;"> Please provide the following' +
+        ' information to complete the study. </p> <div style="text-' +
+        'align:center;"> <div style="text-align:left; display:' +
+        'inline-block; margin-right:20px; line-height:1.8em;"> <ol>' +
+            '<li>Age:</li> <br>' +
+            '<li>Gender:</li> <br><br>' +
+            '<li>Race:</li> <br><br><br><br><br><br>' +
+            '<li>Ethnicity:</li>' +
+        '</ol> </div>' +
+        '<div style="text-align:left; display: inline-block;' +
+        ' line-height:1.8em;">' +
+            '<input name="age" type="number"  min="18" max="100" required /> <br> <br>' +
+            '<input name="gender" type="radio" id="female" value=' +
+                '"Female" required /> <label for="female"> Female </label>' +
+            '<input name="gender" type="radio" id="male" value=' +
+                '"Male" /> <label for="male"> Male </label>' +
+            '<input name="gender" type="radio" id="nonbinary" value=' +
+                '"Non-binary" /> <label for="nonbinary"> Non-binary </label> <br>' +
+            '<input name="gender" type="radio" id="other_gender" value=' +
+                '"other_gender" /> <label for="other_gender"> Other: <input' +
+                ' type="text" name="other_gender" /> </label> <br><br>' +
+            '<input name="race" type="radio" id="white" value=' +
+                '"White" required /> <label for="white"> White </label> <br>' +
+            '<input name="race" type="radio" id="black" value=' +
+                '"Black/African American" /> <label for="black">' +
+                ' Black/African American </label> <br>' +
+            '<input name="race" type="radio" id="am_ind" value=' +
+                '"American Indian/Alaska Native" /> <label for="am_ind">' +
+                ' American Indian/Alaska Native </label> <br>' +
+            '<input name="race" type="radio" id="asian" value=' +
+                '"Asian" /> <label for="asian"> Asian </label> <br>' +
+            '<input name="race" type="radio" id="pac_isl" value=' +
+                '"Native Hawaiian/Pacific Islander" /> <label for="pac_isl">' +
+                ' Native Hawaiian/Pacific Islander </label> <br>' +
+            '<input name="race" type="radio" id="other_race" value="other_race" />' +
+                '<label for="other_race"> Other: <input type="text"' +
+                'name="other_race" /> </label> <br><br>' +
+            '<input name="ethnicity" type="radio" id="hisp" value=' +
+                '"Hispanic" required /> <label for="hisp"> Hispanic </label>' +
+            '<input name="ethnicity" type="radio" id="nonhisp" value=' +
+                '"Non-Hispanic" /> <label for="nonhisp"> Non-Hispanic' +
+                ' </label>' +
+        '</div> </div>' +
+        '<p> Please press the finish button to complete the experiment. </p> </div>',
+    button_label: 'Finish'
+};
---
+++ non_verbal_omission/js/intro.js
@@ -0,0 +1,8 @@
+const intro = {
+    type: jsPsychHtmlButtonResponse,
+    stimulus: '<img src="images/intro.png" style="max-width: 80%; height: auto; border-radius: 8px; box-shadow: 0 4px 12px rgba(0,0,0,0.1);" />',
+    choices: ['next'],
+    data: {
+        page_type: 'intro'
+    }
+};
